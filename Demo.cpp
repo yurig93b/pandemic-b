30bf2ad807457262bb007f8d93943452b0ad02a5
Grigorian <yuri.grigorian@broadcom.com>
2021-05-18 18:05:47 +0300

    empty demo

---
+++ Demo.cpp
@@ -0,0 +1,24 @@
+/**
+ * Demo program for Pandemic exercise - OperationsExpert role
+ * 
+ * Author: Erel Segal-Halevi
+ * Since : 2021-04
+ */
+
+#include "sources/Board.hpp"
+#include "sources/City.hpp"
+#include "sources/Color.hpp"
+
+#include "sources/OperationsExpert.hpp"
+
+using namespace pandemic;
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+int main() {
+	Board board;  // Initialize an empty board (with 0 disease cubes in any city).
+//	board.load_cities();
+}
+
